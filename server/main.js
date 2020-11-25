cfdcc6467f1f4cec4001727a0bd3545ee6d6dc3e
David Song <david.song0802@gmail.com>
2020-11-25 00:49:39 -0500

    socket.io intro

---
+++ server/main.js
@@ -0,0 +1,42 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var path = require('path');
+var io = require('socket.io')(http);
+
+const users = new Map();
+var nextId = 1;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+  console.log(req.method, req.headers.host, req.url);
+});
+
+app.get('/index', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+  console.log(req.method, req.headers.host, req.url);
+});
+
+io.on('connection', (socket) => {
+  // begin: critical section
+  var userId = nextId;
+  users.set(userId, 0);
+  nextId += 1;
+  // end: critical section
+  console.log('User', userId, 'connected');
+  socket.on('disconnect', () => {
+    console.log('User', userId, 'disconnected.');
+    users.delete(userId);
+  });
+  socket.on('chat message', (msg) => {
+    console.log('User', userId, 'says', msg);
+    users.set(userId, users.get(userId)+1);
+    io.emit('chat message', {
+      user: userId,
+      message: msg,
+    });
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
